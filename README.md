49994e1d52a04b8b50c1e9b0b07ff02f01b2c30b
PiYa Nattawud <piya23300@gmail.com>
2016-09-15 15:50:03 +0700

    update readme

--- README.md
+++ README.md
@@ -2,6 +2,38 @@
 [![Code Climate](https://codeclimate.com/github/glazziq/delayed-jobs-management/badges/gpa.svg)](https://codeclimate.com/github/glazziq/delayed-jobs-management)
 [![Test Coverage](https://codeclimate.com/github/glazziq/delayed-jobs-management/badges/coverage.svg)](https://codeclimate.com/github/glazziq/delayed-jobs-management/coverage)
 
-= DelayedJobsManagement
+# DelayedJobsManagement
+
+## Usage
+
+1. install gem
+
+```ruby
+gem 'delayed_jobs_management'
+```
+
+2. add routes
+
+```ruby
+  mount DelayedJobsManagement::Engine => "/delayed_job"
+```
+
+3. *Option* recurring jobs management
+
+```ruby
+# config/initailzers/delayed_jobs_management.rb
+
+DelayedJobsManagement.config do |c|
+  c.recurring_jobs = [
+    { name: 'job_name_1', job: Recurring::ExampleFirstJob },
+    { name: 'job_name_2', job: Recurring::ExampleSecondJob }
+  ]
+end
+```
+
+|attr|desc|
+|----------|----------|
+|`name`| name of recurring job that you want|
+|`job`| class name of recurring job|
 
 This project rocks and uses MIT-LICENSE.
--- app/controllers/delayed_jobs_management/application_controller.rb
+++ app/controllers/delayed_jobs_management/application_controller.rb
@@ -1,40 +1,38 @@
-module DelayedJobsManagement
-  class ApplicationController < ActionController::Base
-    layout 'layouts/delayed_jobs_management/application'
+class DelayedJobsManagement::ApplicationController < ActionController::Base
+  layout 'layouts/delayed_jobs_management/application'
 
-    private
-    def delayed_job
-      begin
-        Delayed::Job
-      rescue
-        false
+  private
+  def delayed_job
+    begin
+      Delayed::Job
+    rescue
+      false
+    end
+  end
+
+  def delayed_jobs(type, queues = [])
+    type = type.to_sym
+
+    rel = delayed_job
+
+    rel =
+      case type
+      when :working
+        rel.where.not(locked_at: nil)
+      when :failed
+        rel.where.not(last_error: nil)
+      when :pending
+        rel.where(attempts: 0, locked_at: nil)
+      else
+        rel.all
       end
-    end
 
-    def delayed_jobs(type, queues = [])
-      type = type.to_sym
+    rel = rel.where(:queue => queues) unless queues.empty?
 
-      rel = delayed_job
+    rel
+  end
 
-      rel =
-        case type
-        when :working
-          rel.where.not(locked_at: nil)
-        when :failed
-          rel.where.not(last_error: nil)
-        when :pending
-          rel.where(attempts: 0, locked_at: nil)
-        else
-          rel.all
-        end
-
-      rel = rel.where(:queue => queues) unless queues.empty?
-
-      rel
-    end
-
-    def set_recurring_jobs
-      @recurring_jobs = DelayedJobsManagement.recurring_jobs.map { |x| OpenStruct.new(x) }
-    end
+  def set_recurring_jobs
+    @recurring_jobs = DelayedJobsManagement.recurring_jobs.map { |x| OpenStruct.new(x) }
   end
 end
